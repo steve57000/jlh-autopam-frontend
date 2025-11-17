bc3727983c5712b2eaa473102a4847432db51ce7
steve <steve57b@hotmail.fr>
2025-11-17 11:43:14 +0100

    update angular v 20.3.2

--- src/main.server.ts
+++ src/main.server.ts
@@ -4,8 +4,8 @@ import localeFr from '@angular/common/locales/fr';
 // enregistrer la locale avant tout
 registerLocaleData(localeFr, 'fr-FR');
 
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-export default () => bootstrapApplication(App, config);
+export default (context: BootstrapContext) => bootstrapApplication(App, config, context);
