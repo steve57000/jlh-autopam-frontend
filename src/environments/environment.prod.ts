497f594d4365904e557323236ffe51340c5f512a
steve <steve57b@hotmail.fr>
2026-01-21 15:07:38 +0100

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://jlh-autopam-backend.onrender.com/api', // URL publique backend prod
-  mediaBaseUrl: 'https://jlh-autopam-backend.onrender.com/uploads',
+  mediaBaseUrl: 'https://jlh-autopam-backend.onrender.com',
 };
