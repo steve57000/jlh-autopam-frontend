b44dd6664c412bce88b4cc8ff7cd4a8e85c423ec
steve <steve57b@hotmail.fr>
2026-01-21 09:37:31 +0100

    updates

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://your-render-service.onrender.com/api',
+  apiBaseUrl: 'https://jlh-autopam-backend.onrender.com/api',
   mediaBaseUrl: 'https://your-render-service.onrender.com',
 };
